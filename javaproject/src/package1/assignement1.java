6db2330ceb6e1225aaf49707cb284586532fa96a
devir <devir@heyansh>
2024-08-09 16:20:32 +0530

    Assignment 1

---
+++ javaproject/src/package1/assignement1.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class assignement1 {int age,rollno;
+
+public void display1() {System.out.println("welcome to all of you");}
+public void display2()
+{
+    System.out.println("Automation is easy");
+}
+
+    public static void main(String[] args) {
+    assignement1 abc = new assignement1();
+            abc.display1();
+            abc.display2();
+            abc.age=10;
+            abc.rollno=32;
+        System.out.println("age="+abc.age);
+        System.out.println("Roll number="+abc.rollno);
+
+    }
+
+}
